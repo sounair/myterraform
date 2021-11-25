00d244d570c3c61b10dd695d107c58dfb6850834
sounair <sounair@gmail.com>
2021-11-25 19:13:51 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_name" {
+    description = "ami value"
+    type = string
+    default= "ami-00be885d550dcee43"
+}
+
+variable "type_name" {
+    description = "type value"
+    type= string
+    default="t2.micro"
+}
