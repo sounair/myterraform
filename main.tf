f2707f12ee1d6e6f721cfbb19de24eee18f4a28d
sounair <sounair@gmail.com>
2021-11-25 19:21:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,23 @@ resource aws_instance soumachine{
     ami=var.ami_name
     count= 3
     instance_type=var.type_name
-    
+
 }
 resource "aws_elb" "bar" {
     name         = "soulb"
     availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
      instances =aws_instance.soumachine.*.id
-}    
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 90
+    lb_protocol       = "http"
+  }
+}
 
 output "lboutput" {
   value = aws_elb.bar.dns_name
+}
+output "op2" {
+  value = aws_instance.soumachine[0].public_ip
 }
